90511bb17e4e6a80ceeb017c6773c6e365e9ef7b
Davide Cristini <davcri91@gmail.com>
2018-01-21 13:45:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# davcri.github.io
---
+++ js/enable-tooltip.js
@@ -0,0 +1,4 @@
+// https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
